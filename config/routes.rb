e375c32e60131b2083106c1432642a8bc6f8cccf
Zuka <zmargvel@chicagobooth.edu>
2017-08-17 04:40:30 +0000

    Fixed Log In

--- config/routes.rb
+++ config/routes.rb
@@ -105,6 +105,11 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+  
+  # Routes for Users
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
